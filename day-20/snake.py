784c7b2aa8f1eb924eadfdfb46bd2429feffb62c
Joan Manel Sancho Garcia <joanmanelsanchogarcia@MacBook-Pro-de-Joan.local>
2024-03-10 19:36:35 +0100

    day-20 finsih

--- day-20/snake.py
+++ day-20/snake.py
@@ -1,12 +1,10 @@
 from turtle import Turtle
-
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
-
 class Snake:
     def __init__(self):
         self.segments = []
@@ -15,11 +13,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
