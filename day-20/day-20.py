87e4b57b7aa77f9d3bc4be1e16758ea1b2aee9fd
Joan Manel Sancho Garcia <joanmanelsanchogarcia@MacBook-Pro-de-Joan.local>
2024-03-24 16:44:33 +0100

    day-24 finish

--- day-20/day-20.py
+++ day-20/day-20.py
@@ -35,12 +35,16 @@ while game_is_on:
 
     #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
+        #scoreboard.game_over()
+        #game_is_on = False
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+            #game_is_on = False
+            #scoreboard.game_over()
 
 screen.exitonclick()
