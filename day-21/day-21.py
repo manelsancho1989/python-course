697f283af796ab3717992b50348200648daf15ca
Joan Manel Sancho Garcia <joanmanelsanchogarcia@MacBook-Pro-de-Joan.local>
2024-03-24 11:51:16 +0100

    day-21 finish

--- day-21/day-21.py
+++ day-21/day-21.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+
 import time
 
 screen = Screen()
@@ -12,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -34,5 +37,14 @@ while game_is_on:
     #detect collisiion with r_paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() > -340:
         ball.bounce_x()
+
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
